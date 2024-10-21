18fb281e299aed2bfffe89e6b9ad9aa4aca295e8
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-21 17:04:50 +0200

    es iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,28 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function stringsFirstLetters(stringsList) {
+  // console.log(stringsList)
+  
+  let newArray = [];
 
+  for (let i = 0; i < names.length; i++) {
+    const currentString = names[i];
+    // console.log(currentString);
+    
+    const stringFirstLetter = currentString.charAt(0);
+    // console.log(stringFirstLetter);
 
+    newArray.push(stringFirstLetter);
+    // console.log(newArray);
+  }
+
+  return newArray;
+};
+  
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const namesFirstLetters = stringsFirstLetters(names)
+console.log(namesFirstLetters)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
