1b8bebabc0d5e203e07ac6117e1f461060d6a8e1
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-21 17:45:40 +0200

    es saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,15 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
+// `Ciao ${string}`
 
 // Dichiara la funzione qui.
-
+const message = (string) => {
+  return `// Ciao ${string}`;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const welcomeMessage = message(name);
+console.log(welcomeMessage);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
