607a4dc3cb44da8e866d8129af2ab850d000ee9f
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-21 17:54:48 +0200

    es bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,27 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
+const message = (string) => {
+  const currentTime = new Date().getHours();
+  
+  if (currentTime < 13) {
+    return `// Buongiorno ${string}`;
+  } 
+  
+  else if (currentTime > 18) {
+    return `// Buonasera ${string}`;
+  } 
+  
+  else {
+    return `// Buon pomeriggio ${string}`;
+  }
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const welcomeMessage = message(name);
+console.log(welcomeMessage);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
